cf758d8e38f9c5fc3d383e31f0254bf79c06a917
Giuliano Machado <giufma@gmail.com>
2019-06-27 15:54:38 -0300

    Updates

---
+++ precache-manifest.d40b4c95145be777bc77a45457dd4172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63635092493d18f40ab5533e2d3e1fd2",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "dd0055d2ad256adf3e70",
+    "url": "/drum-machine/static/css/main.8f1eb818.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/drum-machine/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dd0055d2ad256adf3e70",
+    "url": "/drum-machine/static/js/main.95107ccd.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  }
+]);
